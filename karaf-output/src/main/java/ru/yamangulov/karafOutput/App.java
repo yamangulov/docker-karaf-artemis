e0bc7cbb2be67938fadf25cf1dfa6e098bf9cc2f
Yamangulov Andrey <yamangulov@gmail.com>
2021-02-25 13:41:26 +0300

    Creating App's and adding dependencies into pom's

---
+++ karaf-output/src/main/java/ru/yamangulov/karafOutput/App.java
@@ -0,0 +1,31 @@
+package ru.yamangulov.karafOutput;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jms.JmsComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+
+import javax.jms.ConnectionFactory;
+
+/**
+ * Created by Andrey.Yamangulov
+ * Date: 25.02.2021
+ * Time: 13:22
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        CamelContext ctx = new DefaultCamelContext();
+        ConnectionFactory cf = new ActiveMQConnectionFactory("user", "111111", "tcp://0.0.0.0:61616");
+        ctx.addComponent("jms", JmsComponent.jmsComponentAutoAcknowledge(cf));
+        ctx.addRoutes(
+                new RouteBuilder() {
+                    @Override
+                    public void configure() throws Exception {
+                        from("jms:queue:activemq/queue/FileTransferQueue").to("file:/tmp/out");
+                    }
+                }
+        );
+        ctx.start();
+    }
+}
